7308485bce3e3d356b736f64f8a747982d61fb63
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-10-31 15:32:56 +0530

    Update auth service

--- docker-compose/auth-service/src/main/java/com/ems/auth_service/auth_service/AuthServiceApplication.java
+++ docker-compose/auth-service/src/main/java/com/ems/auth_service/auth_service/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ems.auth_service;
+package com.ems.auth_service.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- docker-compose/auth-service/src/test/java/com/ems/auth_service/auth_service/AuthServiceApplicationTests.java
+++ docker-compose/auth-service/src/test/java/com/ems/auth_service/auth_service/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ems.auth_service;
+package com.ems.auth_service.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
